3b2a6f3f4370aaca7956719c37ecf0475a8f64a5
flyingking888 <jeremyfeiwang@outlook.com>
2024-04-16 01:09:21 -0400

    Updated

--- SeeFood-CoreML/ViewController.swift
+++ SeeFood-CoreML/ViewController.swift
@@ -4,17 +4,18 @@
 //
 //  Created by Angela Yu on 27/06/2017.
 //  Copyright © 2017 Angela Yu. All rights reserved.
-//
+//  Updated by Jeremy Wang 4/10/2024
 
 import UIKit
 import CoreML
 import Vision
 import Social
 
+
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
-    var classificationResults : [VNClassificationObservation] = []
+    var classificationResults : [VNClassificationObservation] = []  //array! :)
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -37,6 +38,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 else {
                     fatalError("unexpected result type from VNCoreMLRequest")
             }
+            //See the model resuts
+            //Use cmd+K to clear the console for testing
+            
+            print(results)
             
             if topResult.identifier.contains("hotdog") {
                 DispatchQueue.main.async {
@@ -47,7 +52,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             else {
                 DispatchQueue.main.async {
-                    self.navigationItem.title = "Not Hotdog!"
+                    //self.navigationItem.title = "Not Hotdog!"
+                    self.navigationItem.title = results[0].identifier
                     self.navigationController?.navigationBar.barTintColor = UIColor.red
                     self.navigationController?.navigationBar.isTranslucent = false
                 }
@@ -66,22 +72,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
+        /*
+         The UIImage is being converted to a CIImage so that it can be used in the detect(image:) method. This custom method utilizes Core Image's capabilities for some form of image analysis or processing, which requires the image to be in the CIImage format.
+         */
+        
+        
         if let image = info[.originalImage] as? UIImage {
             
-            imageView.image = image
+            imageView.image = image // only for confirmation purpose!
             imagePicker.dismiss(animated: true, completion: nil)
             guard let ciImage = CIImage(image: image) else {
                 fatalError("couldn't convert uiimage to CIImage")
             }
-            detect(image: ciImage)
+	            detect(image: ciImage)  //custom method
         }
     }
     
     
     @IBAction func cameraTapped(_ sender: Any) {
         
-        imagePicker.sourceType = .camera
-        imagePicker.allowsEditing = false
+        //Camera is disabled for the simulator
+        
+        //imagePicker.sourceType = .camera
+        imagePicker.sourceType =  UIImagePickerController.SourceType.photoLibrary
+        imagePicker.allowsEditing = true
         present(imagePicker, animated: true, completion: nil)
     }
     
